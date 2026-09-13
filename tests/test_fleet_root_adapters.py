342d54333563b0202a6a6e65e5e07973bc4d8bc8
belilovsky <a.belilovsky@gmail.com>
2026-09-13 11:00:33 +0500

    test(controller): format activation failure assertion

--- tests/test_fleet_root_adapters.py
+++ tests/test_fleet_root_adapters.py
@@ -86,9 +86,7 @@ def test_activation_payload_failure_stage_is_closed_vocabulary() -> None:
         == "activation_runtime_health_failed"
     )
     assert (
-        ACTIVATION._payload_failure_code(
-            "qdev_activation_failure_stage=broker_state\n"
-        )
+        ACTIVATION._payload_failure_code("qdev_activation_failure_stage=broker_state\n")
         == "activation_broker_state_failed"
     )
     assert ACTIVATION._payload_failure_code("qdev_activation_failure_stage=unknown\n") is None
